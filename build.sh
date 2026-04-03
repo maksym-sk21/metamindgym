1f52624ec7205b024c3af511977d546eb0c69420
Maksym <wery5859@gmail.com>
2026-04-03 20:30:31 +0300

    initial commit

--- build.sh
+++ build.sh
@@ -2,5 +2,9 @@
 set -o errexit
 
 pip install -r requirements.txt
+
 python manage.py collectstatic --no-input
+python manage.py makemigrations accounts
+python manage.py makemigrations courses
 python manage.py migrate
+ 
