18ea8a8db7532b2f61f5976ae0e58dfa28ca5d41
Maksym <wery5859@gmail.com>
2026-04-10 14:41:30 +0300

    1 deploy for render

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate --fake-initial
+python manage.py migrate courses 0003 --fake
 python manage.py migrate
 python manage.py create_admin
